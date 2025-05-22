0bed76b1c5cd43399a3c867b7af1b3b071fe769c
GermanS <german.semenkov@gmail.com>
2025-05-22 16:08:15 +0300

    various

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@ mod models;
 mod schema;
 
 use anyhow::{Ok, Result};
-use axum::{Extension, Json, Router, extract::Path, http::StatusCode, routing::get};
-use diesel::{
-    Insertable, RunQueryDsl, SelectableHelper, SqliteConnection,
-    associations::HasTable,
-    r2d2::{self, ConnectionManager, Pool},
-    result,
+use axum::{
+    Extension, Json, Router,
+    extract::Path,
+    response::IntoResponse,
+    routing::{get, post},
 };
-use models::{Device, House, NewDevice, NewHouse, Room};
+use diesel::{
+    ExpressionMethods, RunQueryDsl, SelectableHelper, SqliteConnection,
+    associations::HasTable,
+    query_dsl::methods::FilterDsl,
+    r2d2::{self, ConnectionManager, Pool},
+};
+use models::{Device, House, NewDevice, NewHouse, NewRoom, Room};
 use std::sync::Arc;
 
 struct AppState {
@@ -26,9 +31,12 @@ async fn main() -> Result<()> {
     let app_state = Arc::new(AppState { pool });
 
     let app = Router::new()
-        .route("/houses", get(list_houses))
-        //.route("/houses/:house_id/rooms", get(get_rooms).post(add_room))
-        //.route("/houses/:house_id/rooms/:room_id/devices", get(get_devices))
+        .route("/houses", get(list_houses).post(add_house))
+        .route("/houses/:house_id/rooms", get(get_rooms).post(add_room))
+        .route(
+            "/houses/:house_id/rooms/:room_id/devices",
+            get(get_devices).post(add_device),
+        )
         .layer(Extension(app_state));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -47,55 +55,90 @@ async fn main() -> Result<()> {
 async fn list_houses(Extension(state): Extension<Arc<AppState>>) -> Json<Vec<House>> {
     use schema::house::dsl::*;
 
-    let mut conn = state.pool.get().expect("cant connect");
+    let mut dbh = state.pool.get().expect("cant connect");
 
-    let results = house.load::<House>(&mut *conn).expect("cant execute");
+    let res = house.load::<House>(&mut *dbh).expect("cant execute");
 
-    Json(results)
+    Json(res)
 }
 
-async fn create_house(
+async fn add_house(
     Extension(state): Extension<Arc<AppState>>,
     Json(new_house): Json<NewHouse>,
 ) -> Json<usize> {
     use schema::house::dsl::house;
 
-    let mut conn = state.pool.get().expect("cant connect");
+    let mut dbh = state.pool.get().expect("cant connect");
 
-    let results = diesel::insert_into(house::table())
+    let res = diesel::insert_into(house::table())
         .values(new_house)
-        .execute(&mut *conn)
+        .execute(&mut *dbh)
         .expect("cant execute");
 
-    Json(results)
+    Json(res)
 }
 
-async fn get_rooms(Path(ousex): Path<String>) -> Json<Vec<String>> {
-    // Запрос комнат из БД
-    //Json(vec!["Kitchen".to_string(), "Bedroom".to_string()])
-    todo!()
+async fn get_rooms(
+    Extension(state): Extension<Arc<AppState>>,
+    Path(house_id): Path<i32>,
+) -> Json<Vec<Room>> {
+    use schema::room::dsl::*;
+
+    let mut dbh = state.pool.get().expect("cant connect");
+
+    let res = room
+        .filter(house.eq(house_id))
+        .load::<Room>(&mut *dbh)
+        .expect("cant execute");
+
+    Json(res)
 }
 
-async fn add_room(Path(house_id): Path<String>, Json(name): Json<String>) -> Json<String> {
-    // Добавление комнаты в дом
-    // Json(format!("Комната '{}' добавлена в дом '{}'", name, house_id))
-    todo!()
+async fn add_room(
+    Extension(state): Extension<Arc<AppState>>,
+    Json(new_room): Json<NewRoom>,
+) -> Json<usize> {
+    use schema::room::dsl::*;
+
+    let mut dbh = state.pool.get().expect("cant connect");
+
+    let res = diesel::insert_into(room::table())
+        .values(new_room)
+        .execute(&mut *dbh)
+        .expect("cant execute");
+
+    Json(res)
 }
 
-async fn get_devices(Path((house_id, room_id)): Path<(String, String)>) -> Json<Vec<String>> {
-    // Получаем список устройств из БД
-    // Json(vec!["TV Socket", "Termometer"].map(|s| s.to_string()))
-    todo!()
+async fn get_devices(
+    Path((house_id, room_id)): Path<(i32, i32)>,
+    Extension(app_state): Extension<Arc<AppState>>,
+) -> Json<Vec<Device>> {
+    let mut dbh = app_state.pool.get().expect("cant connect");
+
+    use schema::device::dsl::*;
+
+    let res = device
+        .filter(room.eq(room_id))
+        .load::<Device>(&mut *dbh)
+        .expect("cant execute");
+
+    Json(res)
 }
 
 async fn add_device(
-    Path((house_id, room_id)): Path<(String, String)>,
-    Json(payload): Json<NewDevice>,
-) -> Json<String> {
-    // Сохраняем устройство в БД
-    // Json(format!(
-    //     "Устройство '{}' добавлено в комнату '{}' дома '{}'",
-    //     payload.name, room_id, house_id
-    // ))
-    todo!()
+    Extension(app_state): Extension<Arc<AppState>>,
+    //../Path((house_id, room_id)): Path<(String, String)>,
+    Json(new_device): Json<NewDevice>,
+) -> Json<usize> {
+    let mut dbh = app_state.pool.get().expect("cant connect");
+
+    use schema::device::dsl::*;
+
+    let res = diesel::insert_into(device::table())
+        .values(new_device)
+        .execute(&mut *dbh)
+        .expect("cant execute");
+
+    Json(res)
 }
